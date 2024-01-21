40c89ac82314f9db7d93bb82544bcbf471dfa3ef
Tanmay Sangtani <tanmaysangtani04@gmail.com>
2024-01-22 00:36:07 +0530

    tweaked middleware code

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -3,20 +3,14 @@ const authorizeMiddleware = (req, res, next) => {
   const authHeader = req.headers["authorization"];
 
   if (authHeader) {
-    const tokenParts = authHeader.split(" ");
+    const token = authHeader;
 
-    if (tokenParts.length === 2 && tokenParts[0].toLowerCase() === "bearer") {
-      const token = tokenParts[1];
-
-      try {
-        const decoded = jwt.verify(token, "abc");
-        req.user = decoded;
-        next(); // Continue to the next middleware or route handler
-      } catch (error) {
-        res.status(401).json({ message: "Invalid token" });
-      }
-    } else {
-      res.status(401).json({ message: "Invalid authorization header format" });
+    try {
+      const decoded = jwt.verify(token, "abc");
+      req.user = decoded;
+      next(); // Continue to the next middleware or route handler
+    } catch (error) {
+      res.status(401).json({ message: "Invalid token" });
     }
   } else {
     res.status(401).json({ message: "Authorization header not provided" });
